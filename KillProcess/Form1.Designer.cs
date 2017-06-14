abee0091fb03d2cd79ada5ca774400f8c3266f40
wangjiankun <wangjiankun@haier.com>
2017-06-14 08:09:06 +0800

    write your meaaage

---
+++ KillProcess/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace KillProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.needKillPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(213, 102);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "KillProcess";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // needKillPort
+            // 
+            this.needKillPort.Location = new System.Drawing.Point(93, 102);
+            this.needKillPort.Name = "needKillPort";
+            this.needKillPort.Size = new System.Drawing.Size(100, 21);
+            this.needKillPort.TabIndex = 1;
+            this.needKillPort.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPwd_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "端口号:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 325);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.needKillPort);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "杀死占用端口的进程";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TextBox needKillPort;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KillProcess/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KillProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            string needKillPortStr = this.needKillPort.Text;
+            if(needKillPortStr.Trim().Length==0)
+            {
+                return;
+            }
+            int port;
+            if (this.isNumberic(needKillPortStr, out port))
+            {
+                LookAndStop(port);
+                MessageBox.Show("操作完成");
+            }
+            else
+            {
+                MessageBox.Show("端口必须是数字！");
+            }
+        }
+
+        //根据端口号，查找该端口所在的进程，并结束该进程
+        private void LookAndStop(int port)
+        {
+            Process pro = new Process();
+            // 设置命令行、参数    
+            pro.StartInfo.FileName = "cmd.exe";
+            pro.StartInfo.UseShellExecute = false;
+            pro.StartInfo.RedirectStandardInput = true;
+            pro.StartInfo.RedirectStandardOutput = true;
+            pro.StartInfo.RedirectStandardError = true;
+            pro.StartInfo.CreateNoWindow = true;
+            // 启动CMD    
+            pro.Start();
+            // 运行端口检查命令    
+            pro.StandardInput.WriteLine("netstat -ano");
+            pro.StandardInput.WriteLine("exit");
+            // 获取结果    
+            Regex reg = new Regex("\\s+", RegexOptions.Compiled);
+            string line = null;
+            string endStr = ":" + Convert.ToString(port);
+            while ((line = pro.StandardOutput.ReadLine()) != null)
+            {
+                line = line.Trim();
+                if (line.StartsWith("TCP", StringComparison.OrdinalIgnoreCase))
+                {
+                    line = reg.Replace(line, ",");
+                    string[] arr = line.Split(',');
+                    if (arr[1].EndsWith(endStr))
+                    {
+                        //Console.WriteLine("4001端口的进程ID：{0}", arr[4]);
+                        int pid = Int32.Parse(arr[4]);
+                        Process pro80 = Process.GetProcessById(pid);
+                        // 处理该进程
+                        pro80.Kill();
+                        break;
+                    }
+                }
+                else if (line.StartsWith("UDP", StringComparison.OrdinalIgnoreCase))
+                {
+                    line = reg.Replace(line, ",");
+                    string[] arr = line.Split(',');
+                    if (arr[1].EndsWith(endStr))
+                    {
+                        //Console.WriteLine("4001端口的进程ID：{0}", arr[3]);
+                        int pid = Int32.Parse(arr[3]);
+                        Process pro80 = Process.GetProcessById(pid);
+                        // 处理该进程
+                        pro80.Kill();
+                        break;
+                    }
+                }
+            }
+            pro.Close();
+        }
+
+        private void txtPwd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)//如果输入的是回车键  
+            {
+                this.btnOk_Click(sender, e);//触发button事件  
+            }
+        }
+
+        private bool isNumberic(string message, out int result)
+        {
+            System.Text.RegularExpressions.Regex rex =
+            new System.Text.RegularExpressions.Regex(@"^\d+$");
+            result = -1;
+            if (rex.IsMatch(message))
+            {
+                result = int.Parse(message);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
